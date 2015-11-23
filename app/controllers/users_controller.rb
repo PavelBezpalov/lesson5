356694de4b1c30757ba3b55e1c23bf1aad63aef8
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-11-23 22:16:06 +0200

    Add style for tags and navbar links active state

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,22 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit]
 
   def new
     @user = User.new
   end
 
-  def edit
-  end
-
   def index
-    @users = User.all
+    @users = User.all.sort_by(&:rating).reverse!
   end
 
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        session[:user_id] = @user.id
-        format.html { redirect_to root_path,
-                      notice: 'User was successfully created.' }
-        format.json { render :show,
-                      status: :created,
-                      location: @user }
-      else
-        flash.now[:alert] = 'Something went wrong. Check errors and try again'
-        format.html { render :new }
-        format.json { render json: @user.errors,
-                      status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user,
-                      notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors,
-                      status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url,
-                    notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      flash.now[:alert] = 'Something went wrong. Check errors and try again'
+      render :new
     end
   end
 
